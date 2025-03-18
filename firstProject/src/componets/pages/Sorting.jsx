d9bd3eaae18a700f39478119910b30df07bc1f17
NevilKiyada <nevilkiyada04@gmail.com>
2025-03-18 18:15:19 +0530

    sorting

--- firstProject/src/componets/pages/Sorting.jsx
+++ firstProject/src/componets/pages/Sorting.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router";
+import { React, useState } from "react";
 
-export default function Search() {
-  const [term, setTerm] = useState("");
-
+export default function Sorting() {
+  const [term, setTerm] = useState("asc");
   const data = [
     {
       id: 1,
@@ -12,7 +10,7 @@ export default function Search() {
       description:
         "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
       category: "men's clothing",
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      image: "https://fakestoreapi.com/img/81fPKd-2AYL.AC_SL1500.jpg",
       rating: {
         rate: 3.9,
         count: 120,
@@ -26,7 +24,7 @@ export default function Search() {
         "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
       category: "men's clothing",
       image:
-        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "https://fakestoreapi.com/img/71-3HjGNDUL.AC_SY879._SX._UX._SY._UY.jpg",
       rating: {
         rate: 4.1,
         count: 259,
@@ -39,7 +37,7 @@ export default function Search() {
       description:
         "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
       category: "men's clothing",
-      image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+      image: "https://fakestoreapi.com/img/71li-ujtlUL.AC_UX679.jpg",
       rating: {
         rate: 4.7,
         count: 500,
@@ -52,7 +50,7 @@ export default function Search() {
       description:
         "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
       category: "men's clothing",
-      image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+      image: "https://fakestoreapi.com/img/71YXzeOuslL.AC_UY879.jpg",
       rating: {
         rate: 2.1,
         count: 430,
@@ -66,7 +64,7 @@ export default function Search() {
       description:
         "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
       category: "jewelery",
-      image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+      image: "https://fakestoreapi.com/img/71pWzhdJNwL.AC_UL640_QL65_ML3.jpg",
       rating: {
         rate: 4.6,
         count: 400,
@@ -79,7 +77,7 @@ export default function Search() {
       description:
         "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
       category: "jewelery",
-      image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+      image: "https://fakestoreapi.com/img/61sbMiUnoGL.AC_UL640_QL65_ML3.jpg",
       rating: {
         rate: 3.9,
         count: 70,
@@ -92,7 +90,7 @@ export default function Search() {
       description:
         "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
       category: "jewelery",
-      image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+      image: "https://fakestoreapi.com/img/71YAIFU48IL.AC_UL640_QL65_ML3.jpg",
       rating: {
         rate: 3,
         count: 400,
@@ -105,7 +103,7 @@ export default function Search() {
       description:
         "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
       category: "jewelery",
-      image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+      image: "https://fakestoreapi.com/img/51UDEzMJVpL.AC_UL640_QL65_ML3.jpg",
       rating: {
         rate: 1.9,
         count: 100,
@@ -118,7 +116,7 @@ export default function Search() {
       description:
         "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
       category: "electronics",
-      image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+      image: "https://fakestoreapi.com/img/61IBBVJvSDL.AC_SY879.jpg",
       rating: {
         rate: 3.3,
         count: 203,
@@ -131,7 +129,7 @@ export default function Search() {
       description:
         "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
       category: "electronics",
-      image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+      image: "https://fakestoreapi.com/img/61U7T1koQqL.AC_SX679.jpg",
       rating: {
         rate: 2.9,
         count: 470,
@@ -139,37 +137,28 @@ export default function Search() {
     },
   ];
 
-
-
-  const searchData = data.filter((item) => 
-    item.title.toLowerCase().includes(term.toLowerCase())
+  const sortedData = data.sort((a, b) =>
+    term == "asc" ? a.price - b.price : b.price - a.price
   );
-  const navigation =useNavigate();
-  const handleSubmit=()=>{
-    navigation("/first",{replace:true});}
-
-
   return (
-
-
-    
     <div>
-      <button onClick={handleSubmit()}>submit</button>
-      <h1>Searching</h1>
-      <input
-        type="text"
-        placeholder="Search here"
+      <select
+        name=""
+        id=""
+        value={term}
         onChange={(e) => setTerm(e.target.value)}
-      />
-
-      {searchData.map((e, i) => {
+      >
+        <option value="asc">Asc</option>
+        <option value="desc">Desc</option>
+      </select>
+      {sortedData.map((e, i) => {
         return (
           <ul key={i}>
             <li>
-              {e.title} , {e.price} , {e.category}
+              {e.title} - {e.price} - {e.category}
             </li>
           </ul>
-        );  
+        );
       })}
     </div>
   );
